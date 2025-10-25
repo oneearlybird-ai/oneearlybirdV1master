4103d4a3fd47d605d6380ec1cb0e6b23de7e9b0a
Your Name <oneearlybird.ai@gmail.com>
2025-10-25 13:09:18 -0400

    fix(types): add swiper bundle declaration

--- apps/web/types/swiper.d.ts
+++ apps/web/types/swiper.d.ts
@@ -3,3 +3,10 @@ declare module "swiper" {
   export default Swiper;
   export const Navigation: any;
 }
+
+declare module "swiper/bundle" {
+  const Swiper: any;
+  export default Swiper;
+}
+
+declare module "swiper/css/bundle";
